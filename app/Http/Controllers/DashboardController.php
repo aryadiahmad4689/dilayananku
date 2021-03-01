d32dfd2a94d757b2988470e678d6a36f981c5d18
padaelocomunity2 <sahrulalfa45@gmail.com>
2021-03-01 19:46:04 +0800

    dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,6 +8,6 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin\dahsboard\index');
+        return view('admin.dahsboard.index');
     }
 }
